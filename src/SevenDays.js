aa634a1106ce4a5d23b0cccf51ba84df7f44f219
Alex Barton <alexmbarton@gmail.com>
2018-04-01 16:27:24 +0100

    Adding initial tests

--- src/SevenDays.js
+++ src/SevenDays.js
@@ -1,16 +1,20 @@
 // @flow
 type DayData = {
-  date: Date,
+  date: string,
   count: number
 };
 
 type SevenDayInput = DayData[];
 
 module.exports = function(data: SevenDayInput) {
+  if (!data || data.length < 7) {
+    return false;
+  }
+
   // Take the dataset, reverse it, split into sevenDays
   const output = [];
   let sevenDayCount = 0;
-  let i = 0;
+  let i = 1;
   let rowArray = [];
   Object.values(data)
     // static values(object: any): Array<mixed>;
@@ -19,21 +23,21 @@ module.exports = function(data: SevenDayInput) {
 
     // Loop through data set
     .forEach(value => {
+      if (i % 7 === 6) {
+        rowArray.push(value.date);
+      }
+
+      sevenDayCount += value.count;
       // After each 7 day period, init new array
       if (i % 7 === 0) {
         // Only after the first iteration, create a new array
         // Note that any part 7 day periods at the end of the data set
         // will be ignored.
-        if (i > 0) {
-          rowArray.push(sevenDayCount);
-          sevenDayCount = 0;
-          output.push(rowArray);
-          rowArray = [];
-        }
-        rowArray.push(value.date);
+        rowArray.push(sevenDayCount);
+        output.push(rowArray);
+        sevenDayCount = 0;
+        rowArray = [];
       }
-
-      sevenDayCount += value.count;
 
       i++;
     });
